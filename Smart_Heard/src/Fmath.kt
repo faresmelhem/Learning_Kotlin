abd0f555daf1edc5ead7de84011cc5d999de55e9
Faris <fares.melhem44@gmail.com>
2023-01-09 08:58:20 +0300

    Delete Smart_Heard/src directory

--- Smart_Heard/src/Fmath.kt
+++
@@ -1,8 +0,0 @@
-var  start:Int=0
-
-fun Int.sum(num:Int):Int{start += num ; return if (num==0) start else sum(num-1)}
-
-//another style :
-//fun recursion(a:Int,b:Int, z:Int):Int= if (a < b) z else recursion(a,b+1,z=z+b)
-
-///////////////////////////////////////
--- Smart_Heard/src/testing.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val repetitionNumber:Int = readln().toInt()
-
-   repeat (repetitionNumber){
-    val luck: Int =(0..99).random()
-    println(luck)}
-}
